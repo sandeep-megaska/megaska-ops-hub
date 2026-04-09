b31d1f03671393f32626d35afd686df20fbd49cf
sandeep-megaska <sandeep.lalu@gmail.com>
2026-04-09 11:06:47 +0300

    Update megaska-otp.js

--- extensions/megaska-otp/assets/megaska-otp.js
+++ extensions/megaska-otp/assets/megaska-otp.js
@@ -81,7 +81,7 @@
   const resumingCartAddForms = new WeakSet();
   const ACCOUNT_FALLBACK_DESKTOP_ID = "megaska-account-fallback-desktop";
   const ACCOUNT_FALLBACK_MOBILE_ID = "megaska-account-fallback-mobile";
-  const DEFAULT_MEGASKA_DASHBOARD_URL = "https://shopify.com/96810074410/account";
+  const DEFAULT_MEGASKA_DASHBOARD_URL = "pages/megaska-account";
 
   const ACCOUNT_TRIGGER_SELECTORS = [
     "[data-megaska-open-login]",
